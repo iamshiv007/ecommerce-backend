8107ead46120f1b915d2201f636a985a318c2570
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-17 13:32:38 +0530

    secure set true

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -10,7 +10,7 @@ const sendToken = (user, statusCode, res) => {
         ),
         httpOnly: true,
         sameSite: "None",
-        secure: false
+        secure: true
     }
 
     res.status(statusCode).cookie('token', token, options).json({
